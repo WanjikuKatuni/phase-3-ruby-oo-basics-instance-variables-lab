344e2599f073e9212a9c409fc4d41707a1cac392
wanjiku <annritawanjiku77@gmail.com>
2022-10-26 06:21:25 +0300

    completed

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,39 @@
+require 'pry'
+
 class Dog
 
+    def name= (dogs_name)
+        @this_dogs_name= dogs_name
+    end 
+
+    def name 
+        @this_dogs_name
+    end
+
 end
+
+lassie= Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
+
+# # setter method
+# class Dog
+#     # setter method
+#     def name=(value)
+#         @name = value
+#     end
+
+#     def name
+#         @name
+#     end
+
+# end
+
+# odie = Dog.new
+
+# odie.name="Odie!"
+
+# puts odie.name
+
+# binding.pry
